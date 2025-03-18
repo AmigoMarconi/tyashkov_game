bec10f93e3927092a580bfb088cc6de8882e196e
LeOntik <leontik2106@gmail.com>
2025-03-18 16:35:12 +0300

    Нормальный репозиторий

--- GAYme/мягков-и-формула-времени/scripts/audio_stream_player_2d.gd
+++ GAYme/мягков-и-формула-времени/scripts/audio_stream_player_2d.gd
@@ -1,5 +1,9 @@
 extends AudioStreamPlayer2D
 
-func _ready() -> void:
-	stream = load("res://sounds/Битва за сортир.mp3")
+@onready var volume_slider = $"../Audio/HBoxContainer/MusicVolumeSlider"
+const SOUND_PATH = "res://sounds/Untitled.mp3"
+	
+func _ready():
+	stream = load(SOUND_PATH)
 	play()
+	
--- GAYme/мягков-и-формула-времени/scripts/menu.gd
+++ GAYme/мягков-и-формула-времени/scripts/menu.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var options = $Options_menu
 @onready var audio = $Audio
 
+#ПЕРЕХОД ГЛАВНОГО МЕНЮ 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		toggle()
@@ -19,7 +20,6 @@ func _on_start_button_pressed() -> void:
 
 func _on_options_button_pressed() -> void:
 	show_and_hide(options, menu)
-	#get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
 
 func show_and_hide(first, second):
 	first.show()
@@ -28,11 +28,11 @@ func show_and_hide(first, second):
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
 	
-func _on_volume_button_pressed() -> void:
-	show_and_hide(audio, options)
-	
 func _on_back_button_pressed() -> void:
 	show_and_hide(menu, options)
+	
+	
+#НАСТРОЙКА МУЗЫКИ
 
 func _on_master_value_changed(value: float) -> void:
 	volume(0, value)
@@ -48,3 +48,7 @@ func _on_sound_fx_value_changed(value: float) -> void:
 
 func _on_back_option_button_pressed() -> void:
 	show_and_hide(options, audio)
+
+	
+func _on_volume_button_pressed() -> void:
+	show_and_hide(audio, options)
