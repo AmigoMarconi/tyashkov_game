bbd7b0513af3202d880d93e17e5944773551689a
AmigoMarconi <holaamigooo14@gmail.com>
2025-03-18 15:48:37 +0300

    Сделал нормальный репозиторий

--- tyashkov and formula of time/scripts/audio_stream_player_2d.gd
+++
@@ -1,5 +0,0 @@
-extends AudioStreamPlayer2D
-
-func _ready() -> void:
-	stream = load("res://sounds/Битва за сортир.mp3")
-	play()
--- tyashkov and formula of time/scripts/back_ground.gd
+++
@@ -1,6 +0,0 @@
-extends ParallaxBackground
-
-var speed = 20
-
-func _process(delta):
-	scroll_offset.x -= speed * delta
