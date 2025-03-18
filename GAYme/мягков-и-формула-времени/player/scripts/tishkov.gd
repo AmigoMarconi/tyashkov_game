9bec0f2e217199b71a4a58349feebea990e6b753
AmigoMarconi <holaamigooo14@gmail.com>
2025-03-18 17:47:39 +0300

    Добавил Tilemap и сортировку файлов

---
+++ GAYme/мягков-и-формула-времени/player/scripts/tishkov.gd
@@ -0,0 +1,18 @@
+class_name Tishkov extends CharacterBody2D
+
+var SPEED : float = 12000.0
+
+func _ready() -> void:
+	pass
+
+func _process(delta):
+	var direction : Vector2 = Vector2.ZERO
+	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+	velocity = direction * SPEED * delta
+	
+	pass
+
+func _physics_process(delta):
+	move_and_slide()
