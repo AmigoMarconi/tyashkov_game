8ddd943ca04167184e46f47e8ae6b078b10bb7c2
AmigoMarconi <holaamigooo14@gmail.com>
2025-05-03 03:32:16 +0300

    Работаю над менеджером сцен

--- GAYme/мягков-и-формула-времени/scripts/LevelPerehod.gd
+++ GAYme/мягков-и-формула-времени/scripts/LevelPerehod.gd
@@ -8,10 +8,43 @@ enum SIDE {LEFT, RIGHT, TOP, BOTTOM}
 
 @export_category("Collision Area Settings")
 
-@export_range(1, 12, 1, "or_greater") var size : int = 2
+@export_range(1, 12, 1, "or_greater") var size : int = 2:
+	set(_v):
+		size = _v
+		_update_area()
 
-@export var side :SIDE = SIDE.LEFT
+@export var side :SIDE = SIDE.LEFT:
+	set(_v):
+		side = _v
+		_update_area()
 
 @export var snap_to_grid : bool = false
 
 @onready var collision_shape : CollisionShape2D = $CollisionShape2D
+
+
+func _ready() -> void:
+	_update_area()
+	pass
+
+func _update_area() -> void:
+	var new_rect : Vector2 = Vector2(16, 16)
+	var new_position : Vector2 = Vector2.ZERO
+	
+	if side == SIDE.TOP:
+		new_rect.x *= size
+		new_position.y -= 8
+	elif side == SIDE.BOTTOM:
+		new_rect.x *= size
+		new_position.y += 8
+	elif side == SIDE.RIGHT:
+		new_rect.y *= size
+		new_position.x += 8
+	elif side == SIDE.LEFT:
+		new_rect.y *= size
+		new_position.x -= 8
+	if collision_shape == null:
+		collision_shape = get_node("CollisionShape2D")
+	
+	collision_shape.shape.size = new_rect
+	collision_shape.position = new_position
