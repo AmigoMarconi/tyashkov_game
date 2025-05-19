e7a16ce087b1c79dc97957bda9d4245874a910b8
LeOntik <leontik2106@gmail.com>
2025-05-19 18:06:04 +0300

    не помню

--- GAYme/мягков-и-формула-времени/scripts/menu/menu.gd
+++ GAYme/мягков-и-формула-времени/scripts/menu/menu.gd
@@ -50,19 +50,53 @@ func _on_volume_button_pressed() -> void:
 func _on_continue_button_pressed() -> void:
 	toggle()
 	
+
 #НАСТРОЙКА ВИДЕО
-func _on_fullscreen_toggled(toggled_on: bool) -> void:
+
+func _ready():
+	#СЕЙВ ВИДЕО
+	var screen_type = Persistence.config.get_value("Video", "Fullscreen")
+	if screen_type == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		%Fullscreen.button_pressed = true
+	
+	var borderless_type = Persistence.config.get_value("Video", "Borderless")
+	if borderless_type:
+		%Borderless.button_pressed = true
+		
+	var vsync_index = Persistence.config.get_value("Video", "Vsync", 0)
+	%Vsync.selected = vsync_index
+	
+	#СЕЙВ ЗВУКА
+	%Master.value = Persistence.config.get_value("Audiio", '0')
+	AudioServer.set_bus_volume_db(0, linear_to_db(%Master.value))
+	
+	%Music.value = Persistence.config.get_value("Audio", '1')
+	AudioServer.set_bus_volume_db(1, linear_to_db(%Music.value))
+	
+	%SoundFX.value = Persistence.config.get_value("Audio", '2')
+	AudioServer.set_bus_volume_db(0, linear_to_db(%SoundFX.value))
+	
+	
+func _on_fullscreen_toggled(toggled_on):
 	if toggled_on:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		Persistence.config.set_value("Video", "fullscreen", DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		Persistence.config.set_value("Video", "fulscreen", DisplayServer.WINDOW_MODE_WINDOWED)
+		
+	Persistence.save_data()
 	
 func _on_borderless_toggled(toggled_on):
 	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, toggled_on)
+	Persistence.config.set_value("Video", "bordless", toggled_on)
+	Persistence.save_data()
 
-func _on_option_button_item_selected(index: int) -> void:
+func _on_vsync_item_selected(index):
 	DisplayServer.window_set_vsync_mode(index)
-	
+	Persistence.config.set_value("Video", "vsync", index)
+	Persistence.save_data()
+
 	
 #НАСТРОЙКА МУЗЫКИ
 
@@ -77,3 +111,8 @@ func _on_music_value_changed(value: float) -> void:
 
 func _on_sound_fx_value_changed(value: float) -> void:
 	volume(2, value)
+
+func set_volume(idx, value):
+	AudioServer.set_bus_volume_db(idx, linear_to_db(value) )
+	Persistence.config.set_value("Audio", str(idx), value)
+	Persistence.save_data()
