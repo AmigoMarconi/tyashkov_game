8142f7f950b1859072553080d732326576c08e17
AmigoMarconi <holaamigooo14@gmail.com>
2025-05-20 20:13:20 +0300

    Z

--- GAYme/мягков-и-формула-времени/00_GLOBAL/global_player.gd
+++ GAYme/мягков-и-формула-времени/00_GLOBAL/global_player.gd
@@ -33,67 +33,6 @@
 	#set_process_input(enabled)
 	#set_process_unhandled_input(enabled)
 
-#extends Node
-#
-#const PLAYER = preload("res://player/tishkov.tscn")
-#var player : Tishkov
-#var player_spawned : bool = false
-#
-#func _ready() -> void:
-	#add_player_instance()
-	#
-	## Подписываемся на сигнал загрузки уровня
-	#LevelMeneger.level_loaded.connect(_on_level_loaded)
-#
-#func add_player_instance() -> void:
-	#if !player:
-		#player = PLAYER.instantiate()
-		#add_child(player)
-		#print("PlayerManager: Игрок создан")
-#
-#func _on_level_loaded() -> void:
-	#print("PlayerManager: Уровень загружен")
-	#
-	## Если это переход через LevelPerehod, то позиция будет установлена 
-	## в методе place_player скрипта LevelPerehod
-	#if LevelMeneger.target_transition != "":
-		#print("PlayerManager: Ожидание установки позиции через LevelPerehod")
-		#return
-		#
-	## Если это начальный запуск или новая игра
-	#if !player_spawned || LevelMeneger.is_first_load:
-		#var spawn_points = get_tree().get_nodes_in_group("player_spawn")
-		#if spawn_points.size() > 0:
-			#set_player_position(spawn_points[0].global_position)
-			#print("PlayerManager: Установлена позиция через точку спавна: ", spawn_points[0].global_position)
-		#else:
-			#print("PlayerManager: ВНИМАНИЕ! Точка спавна не найдена!")
-			#set_player_position(Vector2(100, 100)) # Запасная позиция
-	#
-	#player_spawned = true
-#
-#func set_player_position(new_pos : Vector2) -> void:
-	#if player:
-		#player.global_position = new_pos
-		#print("PlayerManager: Позиция игрока установлена на ", new_pos)
-#
-#func set_as_parent(p : Node2D) -> void:
-	#if player and player.get_parent():
-		#player.get_parent().remove_child(player)
-	#if p:
-		#p.add_child(player)
-		#print("PlayerManager: Игрок прикреплен к ", p.name)
-#
-#func unparent_player(p : Node2D) -> void:
-	#if p and player and player.get_parent() == p:
-		#p.remove_child(player)
-		#print("PlayerManager: Игрок откреплен от ", p.name)
-#
-#func set_controls_enabled(enabled: bool):
-	#if player:
-		#player.set_process_input(enabled)
-		#player.set_process_unhandled_input(enabled)
-
 extends Node
 const PLAYER = preload("res://player/tishkov.tscn")
 var player : Tishkov
